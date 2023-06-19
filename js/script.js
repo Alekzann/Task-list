f9907b487856e3f5a69060587576a1ffd97770d5
Oleksandr Riaboshapka <hadion@mail.ru>
2023-06-19 15:03:24 +0200

    remove tasks

--- js/script.js
+++ js/script.js
@@ -11,26 +11,42 @@
 
 	];
 
-	const render = () => {
-		let htmlString = "";
-
-		for (const task of tasks) {
-			htmlString += `
-            <li${task.done ? " class=\"contentDone\"" : ""}>
-            ${task.content}
-            </li>
-            `;
-		};
-
-		document.querySelector(".js-tasks").innerHTML = htmlString;
-	};
-
 	const addNewTask = (newTaskContent) => {
 		tasks.push({
 			content: newTaskContent,
 		});
 		render();
 	};
+
+	const removeTask = (index) => {
+		tasks.splice(index, 1);
+				render();
+	}
+
+	const render = () => {
+		let htmlString = "";
+
+		for (const task of tasks) {
+			htmlString += `
+            <li${task.done ? " class=\"contentDone\"" : ""}>
+            <button class="js-remove">usuń</button>
+						${task.content}
+            </li>
+            `;
+		};
+
+		document.querySelector(".js-tasks").innerHTML = htmlString;
+
+		const removeButtons = document.querySelectorAll(".js-remove");
+
+		removeButtons.forEach((removeButton, index) =>{
+			removeButton.addEventListener("click", () => {
+				removeTask(index);
+			});
+		});
+	};
+
+	
 
 	const onFormSubmit = (event) => {
 		event.preventDefault();
